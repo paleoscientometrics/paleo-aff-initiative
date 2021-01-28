2f30a0f35662eb1797a6c2b2005ac96c6c65f090
Nussaibah Raja <nussaibah.raja.schoob@fau.de>
2021-01-28 19:02:42 +0100

    [scripts] script added to get updated dataset from the PBDB

---
+++ scripts/update_affs.R
@@ -0,0 +1,16 @@
+# Download and merge new references ---------------------------------------
+
+# API: https://paleobiodb.org/data1.2/occs/refs.csv?interval=Ediacaran,Holocene&select=occs
+
+# Get data file for completed affiliations
+old_refs <- read.csv(file.path("data_archive", "aff-data-complete.csv"))
+
+new_refs <- read.csv("https://paleobiodb.org/data1.2/occs/refs.csv?interval=Ediacaran,Holocene&select=occs",
+					 encoding = "UTF-8")
+
+# which ones haven't been done yet
+todo_refs <- new_refs[!new_refs$reference_no %in% old_refs$reference_no,]
+todo_refs <- todo_refs[!is.na(todo_refs$pubyr),]
+
+# save data
+write.csv(todo_refs, file.path("data_archive", "aff_data-todo.csv"), row.names = FALSE)
